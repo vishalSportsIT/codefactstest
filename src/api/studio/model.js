ca84488d173b6297e116276546acf8f6afb3d8e2
vishal <vishal.mudavath@negroup.com>
2024-11-04 18:28:29 +0530

    modified model

--- src/api/studio/model.js
+++ src/api/studio/model.js
@@ -1,4 +1,3 @@
-// src/api/studio/model.js
 
 import mongoose from 'mongoose';
 
@@ -6,15 +5,12 @@ const studioSchema = new mongoose.Schema({
     name: { type: String, required: true },
     location: { type: String, required: true },
     description: { type: String, required: true },
-    pricing: { type: Number, required: true },
-    availability: { type: Boolean, default: true },
-    contactInfo: {
-        phone: { type: String, required: true },
-        email: { type: String, required: true },
-    },
-    category: { type: String, required: true },
+    pricing: { type: String, required: true }, 
+    availability: { type: String, required: true }, 
+    contactInfo: { type: String, required: true }, 
+    category: { type: String, required: true }, 
     rating: { type: Number, min: 0, max: 5 },
-    image: { type: String }, // Added field for storing image path
+    image: { type: String }, 
 }, { timestamps: true });
 
 const Studio = mongoose.model('Studio', studioSchema);
